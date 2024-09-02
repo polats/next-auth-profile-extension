6ebbe1d56a9f7ec2439387f385bbf1b6997ec8e5
Paul Gadi <paulgadi@gmail.com>
2024-09-02 15:22:01 -0400

    Update ScaffoldEthAppWithProviders.tsx.args.mjs

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,9 +1,9 @@
-export const providerImports = `
+export const providerImports = `{
 import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { SessionProvider } from "next-auth/react";
 }`;
 
-export const providerNames = {
-  "SessionProvider": "SessionProvider",
-  "RainbowKitSiweNextAuthProvider": "RainbowKitSiweNextAuthProvider",
-};
+export const providerNames = [
+  "SessionProvider",
+  "RainbowKitSiweNextAuthProvider"
+];
